1e30e42b875c079e9cf515662dcafe5fc2662299
Camilo Andres Ramos Grajales <camilo.a.ramos@hotmail.com>
2026-01-17 21:50:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -42,16 +42,3 @@ function formatProductPrices() {
         element.textContent = '$' + formattedPrice;
     });
 }
-// Delegación de eventos para las imágenes del carrusel
-document.querySelectorAll('.product-carousel').forEach(carousel => {
-    carousel.addEventListener('click', (e) => {
-        // Verificar si el clic fue en una imagen
-        const img = e.target.closest('.carousel-slide img');
-        if (img) {
-            e.preventDefault();
-            const productId = parseInt(img.getAttribute('data-product'));
-            const slideIndex = parseInt(img.getAttribute('data-slide'));
-            openImageModal(productId, slideIndex);
-        }
-    });
-});
